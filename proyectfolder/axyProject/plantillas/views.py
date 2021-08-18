f7ac57c2cfe3360391f47a30a3021303b5c4daa1
naxyam <naxyam@hotmail.com>
2021-08-18 18:13:41 -0500

    creación de modelos

--- proyectfolder/axyProject/plantillas/views.py
+++ proyectfolder/axyProject/plantillas/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from plantillas.models import Departamento, Beneficiario, Ciudades, Grupoetareo,
+from django.utils import timezone
+from 
 
+def getFichaSocialRegistro(request):
+    if request.method == 'POST':
+        depa = Departamento.objects.all()
+        bene = Beneficiario.objects.all()
+        ciu = Ciudades.objects.all()
+        grupeta = Grupoetareo.objects.all()
+        if formulario_contacto.is_valid():
+            nombre= request.POST.get("nombre")
+            email= request.POST.get("email")
+            asunto= request.POST.get("asunto")
+            mensaje= request.POST.get("mensaje")
 
-def plantillas(request):
-    return render(request, "plantillas/adminPlantillas.html")
+            
 
-def fichaSocReg(request):
-    return render(request, "plantillas/adminFicha.html")
+    return render(request, "contacto/contacto.html",{"miformulario":formulario_contacto})
+
 
 def guardarInfo(request):
     benef_id=request.POST['']
