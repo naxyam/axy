f8a1f15c2d7d898b2aebd7a605ef24dc9b4e1751
naxyam <naxyam@hotmail.com>
2021-08-28 10:28:48 -0500

    creacion de beneficiario

--- proyectfolder/axyProject/plantillas/views.py
+++ proyectfolder/axyProject/plantillas/views.py
@@ -3,29 +3,28 @@ from django.http.response import HttpResponseRedirect
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.urls import reverse
+from django.urls.base import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 from plantillas.forms import formFichaReg
+from plantillas.models import Beneficiario
 
 
 def plantillas(request):   
     return render(request, 'plantillas/adminPlantillas.html')
 
-
-def fichaSocReg2(request):   
-    return render(request, 'adminFicha2.html')
-
-
 class fichaSocReg(CreateView):
+    model=Beneficiario
     form_class = formFichaReg
-    template_name = 'plantillas/adminFicha.html'  
+    template_name = 'plantillas/adminFicha.html' 
+    success_url = reverse_lazy('plantillas: success') 
 
-    def form_valid(self, form):
-        beneficiario = form['formFichaReg'].save(commit=False)
-        beneficiario.save()
-        return HttpResponseRedirect(reverse('plantillas/adminFicha2'))
+'''def fichaSocReg2(request):   
+    return render(request, 'plantillas/adminFicha2.html')'''
 
-class SuccessView(TemplateView):
-    template_name = 'plantillas/adminFicha2.html'
+def success(request):   
+    return render(request, 'plantillas/success.html')
+
+
 
 ''''
 
